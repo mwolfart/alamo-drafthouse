37e2d27949fccdf2c25be6780b84e00ab4be1708
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-05-14 11:43:00 -0300

    Add data types

---
+++ src/types/data.ts
@@ -0,0 +1,15 @@
+export type Cinema = {
+    id: string
+    name: string
+}
+
+export type Film = {
+    title: string
+    slug: string
+}
+
+export type Session = {
+    cinemaId: string
+    filmSlug: string
+    filmTitle: string
+}
