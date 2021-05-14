b2878f7aabdd30d727d5a5bd147b16c6161409c1
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-05-14 11:48:49 -0300

    Add correct module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BackgroundDetailComponent } from 'src/components/background-detail/background-detail.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MenuTheaterComponent } from 'src/components/menu-theater/menu-theater.component';
+import { ListFilmsComponent } from 'src/components/list-films/list-films.component';
+import { ActionButtonComponent } from 'src/components/action-button/action-button.component';
+import { CustomLinkComponent } from 'src/components/custom-link/custom-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundDetailComponent,
+    ListFilmsComponent,
+    ActionButtonComponent,
+    CustomLinkComponent,
+    MenuTheaterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
