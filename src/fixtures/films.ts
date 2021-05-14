2f6988675a0ad9535c98c9b213fc8104d2d51d45
Murilo Wolfart <murilo.wolfart@getambush.com>
2021-05-14 11:46:10 -0300

    Add fixtures for testing

---
+++ src/fixtures/films.ts
@@ -0,0 +1,9 @@
+import { Film } from 'src/types/data';
+
+export const films: Film[] = [
+    { title: 'Mad Max', slug: 'mad-max' },
+    { title: 'Downtown Abbey', slug: 'downtown-abbey' },
+    { title: 'Joker', slug: 'joker' },
+    { title: 'Ad Astra', slug: 'ad-astra' },
+    { title: 'Hustlers', slug: 'hustlers' },
+]
---
+++ src/fixtures/sessions.ts
@@ -0,0 +1,10 @@
+import { Session } from 'src/types/data';
+
+export const sessions: Session[] = [
+    { cinemaId: '0001', filmTitle: 'Mad Max', filmSlug: 'mad-max' },
+    { cinemaId: '0001', filmTitle: 'Downtown Abbey', filmSlug: 'downtown-abbey' },
+    { cinemaId: '0002', filmTitle: 'Downtown Abbey', filmSlug: 'downtown-abbey' },
+    { cinemaId: '0002', filmTitle: 'Ad Astra', filmSlug: 'ad-astra' },
+    { cinemaId: '0002', filmTitle: 'Hustlers', filmSlug: 'hustlers' },
+    { cinemaId: '0003', filmTitle: 'Joker', filmSlug: 'joker' },
+]
